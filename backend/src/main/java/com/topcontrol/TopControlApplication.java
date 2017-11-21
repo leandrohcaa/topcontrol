6721211ed96dd827768a920005aab490ed9bc880
Z0035VWW <Z0035VWW@PC006885.pmt.primetals.net>
2017-11-20 23:23:36 -0200

    Commit inicial.

---
+++ backend/src/main/java/com/topcontrol/TopControlApplication.java
@@ -0,0 +1,12 @@
+package com.topcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopControlApplication.class, args);
+	}
+}
